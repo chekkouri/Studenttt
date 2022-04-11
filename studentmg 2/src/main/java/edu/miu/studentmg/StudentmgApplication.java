aa85e499caee780c8ca8ee9f278c318a11c7126d
CHEKKOURI Fatima ezzahraa <61733267+chekkouri@users.noreply.github.com>
2022-04-11 00:16:07 -0500

    Add files via upload

---
+++ studentmg 2/src/main/java/edu/miu/studentmg/StudentmgApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.studentmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmgApplication.class, args);
+	}
+
+}
---
+++ studentmg 2/src/test/java/edu/miu/studentmg/StudentmgApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.studentmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
